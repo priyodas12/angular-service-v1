d6a295c682fa7b6ffd3e1479e4887f90e9382e35
Priyobrato.Das <priyodas12@gmail.com>
2023-12-16 12:35:00 +0530

    basic structure and header subscription

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AdminComponent } from './header/admin/admin.component';
+import { HomeComponent } from './header/home/home.component';
+import { HeroComponent } from './header/home/hero/hero.component';
+import { SidebarComponent } from './header/home/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AdminComponent,
+    HomeComponent,
+    HeroComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
